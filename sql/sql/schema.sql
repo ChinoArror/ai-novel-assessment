e009888344c93882589ec5efc09775020a2d00ff
Chino Arror <103983438+ChinoArror@users.noreply.github.com>
2026-01-17 02:02:22 +0800

    Delete sql directory

--- sql/sql/schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS essays;
-CREATE TABLE essays (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    timestamp TEXT NOT NULL,
-    topic TEXT,
-    image_key TEXT,
-    essay_type TEXT,
-    grade_result TEXT
-);
